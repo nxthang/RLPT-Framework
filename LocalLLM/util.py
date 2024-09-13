797ab7381ffa70f08e9b755d3fdb066a8fbbc0c1
nxthang <75068100+nxthang@users.noreply.github.com>
2024-09-13 19:34:07 +0700

    Update util.py

--- LocalLLM/util.py
+++ LocalLLM/util.py
@@ -2,6 +2,7 @@
 import os
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
+import math
 import keras
 import keras_nlp
 from keras import ops
@@ -196,3 +197,35 @@ def build_dataset(words, labels=None, return_ids=False, batch_size=4,
     ds = ds.batch(batch_size, drop_remainder=drop_remainder)  # batch dataset
     ds = ds.prefetch(AUTO)  # prefetch next batch
     return ds
+
+# Learning rate Schedule
+def get_lr_callback(batch_size=8, mode='cos', epochs=10, plot=False):
+    lr_start, lr_max, lr_min = 6e-6, 2.5e-6 * batch_size, 1e-6
+    lr_ramp_ep, lr_sus_ep, lr_decay = 3, 0, 0.75
+
+    def lrfn(epoch):  # Learning rate update function
+        if epoch < lr_ramp_ep: lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start
+        elif epoch < lr_ramp_ep + lr_sus_ep: lr = lr_max
+        elif mode == 'exp': lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
+        elif mode == 'step': lr = lr_max * lr_decay**((epoch - lr_ramp_ep - lr_sus_ep) // 2)
+        elif mode == 'cos':
+            decay_total_epochs, decay_epoch_index = epochs - lr_ramp_ep - lr_sus_ep + 3, epoch - lr_ramp_ep - lr_sus_ep
+            phase = math.pi * decay_epoch_index / decay_total_epochs
+            lr = (lr_max - lr_min) * 0.5 * (1 + math.cos(phase)) + lr_min
+        return lr
+
+    if plot:  # Plot lr curve if plot is True
+        fig = px.line(x=np.arange(epochs),
+                      y=[lrfn(epoch) for epoch in np.arange(epochs)], 
+                      title='LR Scheduler',
+                      markers=True,
+                      labels={'x': 'epoch', 'y': 'lr'})
+        fig.update_layout(
+            yaxis = dict(
+                showexponent = 'all',
+                exponentformat = 'e'
+            )
+        )
+        fig.show()
+
+    return keras.callbacks.LearningRateScheduler(lrfn, verbose=False)  # Create lr callback
